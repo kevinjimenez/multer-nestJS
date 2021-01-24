f95a8da2ae9b079d48dadfa954ef55100284ac17
kevin <kevin@DESKTOP-R3TH26J.localdomain>
2021-01-24 15:08:24 -0500

    upload

--- ejemplo-multer/src/app.controller.ts
+++ ejemplo-multer/src/app.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +19,41 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // @Post('upload')
+  // @UseInterceptors(FilesInterceptor('image'))
+  // uploadFile(@UploadedFiles() file) {
+  //   console.log(file);
+  // }
+
+  @Post('subir-imagen')
+  @UseInterceptors(FileInterceptor('archivo'))
+  async subirImagenes(
+    @UploadedFile() archivo,
+    // @Body('idImagen') idImagen
+    @Req() req
+    ) {
+      console.log(req.files);
+      //console.log(Object.keys(req.body));
+      console.log(archivo);
+    try {
+      // console.log(archivo);
+      return { archivo };
+      // return await this._imagenService
+      //     .subirImagen(archivo, idImagen);
+      // return await this._vtaDifareFarmService
+      //     .convertirArchivoExcelACsv(archivo);
+    } catch (e) {
+      console.error({
+        error: e,
+        mensaje: 'Error al leer el archivo',
+        data: { archivo },
+      });
+      throw new BadRequestException({
+        mensaje: 'Error en archivo',
+        error: e,
+      });
+    }
   }
 }
--- ejemplo-multer/src/main.ts
+++ ejemplo-multer/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.useStaticAssets(`${__dirname}/public`);
+  // the next two lines did the trick
+  // app.use(bodyParser.json({limit: '50mb'}));
+  // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(3000);
 }
 bootstrap();
